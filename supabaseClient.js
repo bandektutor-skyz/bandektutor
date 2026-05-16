1d272acf69e19e35a0ad69bf27998170eb50d9f7
promcareservice <bandektutor@gmail.com>
2026-05-16 11:58:13 +0700

    update fresh supabase api key

--- supabaseClient.js
+++ supabaseClient.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 // กุญแจและที่อยู่เซิร์ฟเวอร์ฐานข้อมูลของโปรเจกต์คุณโดยตรง
 const supabaseUrl = 'https://tapfpgdaaidyrbmfouds.supabase.co';
-const supabaseAnonKey = 'sb_publishable_fLoEfuAcX0bQabvcQGxxkQ_c_XNWlo3';
+const supabaseAnonKey = 'sb_publishable_fLoEfuAcX0bQabvcQGxckQ_c_XNWIo3';
 
 // สร้างฟังก์ชันตัวเชื่อมต่อเพื่อส่งออก (Export) ไปให้หน้าอื่นๆ เรียกใช้งาน
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
